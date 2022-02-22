7f8530b7371ecbd988e27a6c62a06f3e10f25106
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-22 14:06:44 +0200

    do task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onClickBtnStart);
+refs.btnStop.addEventListener('click', onClickBtnStop);
+
+function onClickBtnStart() {
+  setRandomColor();
+
+  timerId = setInterval(() => {
+    setRandomColor();
+  }, 1000);
+
+  refs.btnStart.setAttribute('disabled', 'disabled');
+  refs.btnStop.removeAttribute('disabled');
+}
+
+function onClickBtnStop() {
+  clearInterval(timerId);
+  refs.btnStop.setAttribute('disabled', 'disabled');
+  refs.btnStart.removeAttribute('disabled');
+}
+
+function setRandomColor() {
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
